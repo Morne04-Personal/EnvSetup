492a68742efe9cecfd6847a24b458a28c06f1763
Morne04 <89552532+Morne04@users.noreply.github.com>
2021-08-26 00:30:36 +0200

    Initial Install
    1. Update & Upgrade
    2. Install Docker Engine
    3. Install Docker Compose
    4. Install Portainer (WebGUI on Port 1000)
    6. Install GIT
    5. Install ZSH
    6. Install Curl
    7. Install Nano
    8. Install UFW
    9. Install hTop
    10. Install Net-Tools

---
+++ First.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apt-get update -y
+apt-get upgrade -y
+apt-get install git -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce docker-compose
+docker volume create portainer_data
+docker run -d -p 1000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+apt-get install zsh curl nano ufw htop net-tools -y 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
